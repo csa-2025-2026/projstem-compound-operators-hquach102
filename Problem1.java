742e4c5371d898fb4e4dd2bfe022989ec283d3c5
hquach102 <hquach102@student.hbuhsd.edu>
2025-09-11 21:06:25 +0000

    finished the assignment

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,20 @@
-
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        int initalNum = 6;
+        System.out.println("Initial value: " + initalNum);
+        initalNum += 1;
+         System.out.println("number is now " + initalNum);
+         initalNum += 1;
+         System.out.println("number is now " + initalNum);
+         initalNum += 1;
+         System.out.println("number is now " + initalNum);
+         initalNum += 1;
+         System.out.println("number is now " + initalNum);
+        int initialNum = 10;
+         initialNum -= 1;
+         System.out.println("number is now " + initalNum);
+    
+    }
+}
